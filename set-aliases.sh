e8c8de75b35c930f33bdc039189f4cd03363e739
David Gray <dacgray@gmail.com>
2021-07-28 11:44:31 +0700

    chore: delete aliases on re-run of set-aliases

--- set-aliases.sh
+++ set-aliases.sh
@@ -4,7 +4,8 @@ set -eE
 AR_PATH=$(realpath assume-role.sh)
 GST_PATH=$(realpath get-session-token.sh)
 
-sed -i '/^#/d' ~/.bashrc || true
+sed -i '/^alias assume-role/d' ~/.bashrc || true
+sed -i '/^alias get-session-token/d' ~/.bashrc || true
 
 echo alias assume-role=\'${AR_PATH}\' >> ~/.bashrc
 echo alias get-session-token=\'${GST_PATH}\' >> ~/.bashrc
